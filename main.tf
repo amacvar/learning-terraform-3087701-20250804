c97a6b9965e024eecd45d309436859415b74046f
Amit <5795880+amacvar@users.noreply.github.com>
2025-08-04 19:33:57 -0400

    add var.instance type to main.tf

--- main.tf
+++ main.tf
@@ -14,9 +14,28 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "example" {
+  filter {
+    name   = "tag:Name"
+    values = ["myvpc"] # Replace with the name of your VPC
+  }
+}
+
+# (Information not from sources: This block would retrieve a subnet within that VPC)
+data "aws_subnet" "example" {
+  vpc_id = data.aws_vpc.example.id
+  filter {
+    name   = "tag:Name"
+    values = ["sn7d"] # Replace with the name of your desired subnet
+  }
+}
+
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = var.instance_type
+  subnet_id = data.aws_subnet.example.id
+  security_groups = ["sg-0640ce76ed6404c3a"]
 
   tags = {
     Name = "HelloWorld"
